19f969a80d3bc7bba437a69749244f130279a435
Aneesh-aj <rajuaneesh.p2020@gmail.com>
2023-12-17 20:38:53 +0530

    added the signin

--- Client/src/pages/Signup.jsx
+++ Client/src/pages/Signup.jsx
@@ -2,27 +2,41 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Signup() {
-      const [formData,setFormData] = useState({})
-     const handleChange= (e)=>{
-          setFormData({...formData,[e.target.id]:e.target.value})
-     }
+    const [formData, setFormData] = useState({})
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(false)
 
-     const handleSubmit= async (e)=>{
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.id]: e.target.value })
+    }
+
+    const handleSubmit = async (e) => {
         e.preventDefault()
-         const res= await fetch('/api/auth/signup',{
-            method:'POST',
-            headers:{
-                'Content-Type':'application/json'
-            },
-            body: JSON.stringify(formData)
-         })
-         const data= await res.json()
-         console.log(data)
-     }
 
+        try {
+            setLoading(true)
+            setError(false)
+            const res = await fetch('/api/auth/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            })
+            const data = await res.json()
+            setLoading(false)
+            if(data.success === false){
+                setError(true)
+                return
+            }
+            setError(false)
+        } catch (error) {
+            setLoading(false)
+            setError(true)
+        }
 
-
-
+    }
 
 
     return (
@@ -30,22 +44,24 @@ export default function Signup() {
             <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
             <form onSubmit={handleSubmit} className="flex flex-col max-w-lg max-auto gap-3 ">
                 <input type="text" placeholder="Username"
-                    id="username"   onChange={handleChange}
+                    id="username" onChange={handleChange}
                     className="bg-slate-100 p-3 rounded-lg" />
                 <input type="email" placeholder="email"
                     id="email" onChange={handleChange}
                     className="bg-slate-100 p-3 rounded-lg" />
                 <input type="password" placeholder="password"
-                    id="password"  onChange={handleChange}
+                    id="password" onChange={handleChange}
                     className="bg-slate-100 p-3 rounded-lg" />
-                    <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"type="submit" >Sign up</button>
+                <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80" type="submit" >
+                    {loading ? 'Loading...':'Sing up'}</button>
             </form>
-              <div className="flex gap-2 mt-5">
-                 <p>have an account ?</p>
-                 <Link to="/sign-in" >
-                 <span className="text-blue-500" >Sign in</span>
-                 </Link>
-              </div>
+            <div className="flex gap-2 mt-5">
+                <p>have an account ?</p>
+                <Link to="/sign-in" >
+                    <span className="text-blue-500" >Sign in</span>
+                </Link>
+            </div>
+            <p className="text-red-700 mt-5">{error&& 'Something went wrong'}</p>
         </div>
     )
 }
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,9 @@
 import express from 'express'
 import { signup } from '../controllers/auth.controller.js'
 
-
 const router = express.Router()
-console.log("here in the route")
 
 router.post('/signup',signup)
+router.post('/signin',)
 
 export default router
